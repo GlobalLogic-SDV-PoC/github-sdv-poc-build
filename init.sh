8b304e394a29c0dd4b49f8916d5ce4b879c6c174
Yurii Holyk <160478622+yurii-holyk@users.noreply.github.com>
2024-02-29 13:54:58 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -15,9 +15,10 @@ dpkg -i packages-microsoft-prod.deb && rm -f packages-microsoft-prod.deb
 echo 'deb [trusted=yes] http://security.ubuntu.com/ubuntu bionic-security main' \
     | tee -a /etc/apt/sources.list.d/bionic-security.list
 
-apt update
-## TODO: WIP. S3 repo with aptly
-apt install -y /dists/*.deb ## Change me ASAP
+echo 'deb [trusted=true] https://he-poc-repo-bucket-b997.s3.eu-west-1.amazonaws.com/ stable main' \
+    | tee -a /etc/apt/sources.list.d/he-poc.list
+
+apt update && apt install -y astemo-meta
 
 update-alternatives --install /usr/bin/python python /usr/bin/python2.7 0
 
@@ -27,11 +28,9 @@ cp -a MakeFile_changed /opt/qcom_qnx/apps/qnx_ap/Makefile
 mkdir -p ${HOME}/.qnx/
 cp -a /opt/qcom_qnx/apps/qnx_ap/qnx_bins/license ${HOME}/.qnx/license
 
-(
-    /app/build.sh build all \
-    && /app/build.sh metabuild \
-    && /app/build.sh collect
-    du -sb ./out_collect
-)
-
-sleep infinity
+## (
+##    /app/build.sh build all \
+##    && /app/build.sh metabuild \
+##    && /app/build.sh collect
+##    du -sb ./out_collect
+## )
